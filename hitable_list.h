5207d233cd5ae190f99537f62c988c84b587bdfb
ivananovakovic1 <ivananovakovic03@gmail.com>
2026-07-14 19:01:59 +0200

    Update hitable_list.h

--- hitable_list.h
+++ hitable_list.h
@@ -3,48 +3,48 @@
 
 #include "hitable.h"
 
-// Ova klasa nam služi da grupišemo sve objekte u sceni na jedno mjesto.
-// Pošto i sama nasljeđuje 'hitable', možemo je tretirati kao jedan veliki objekat za provjeru sudara.
-class hitable_list: public hitable {
+// hitable_list je klasa koja predstavlja kolekciju (grupu) 3D objekata.
+// Nasljeđuje 'hitable' interfejs, što joj omogućava da se ponaša kao jedan jedinstveni objekat u sceni.
+class hitable_list: public hitable  {
     public:
-        hitable_list() {}
+        // CUDA priručnik zahtijeva specifikator '__device__' kako bi se konstruktor 
+        // mogao pozvati unutar GPU kernela prilikom kreiranja objekata na grafičkoj kartici.
+        __device__ hitable_list() {}
         
-        // Konstruktor prima niz pokazivača na hitable objekte (l) i veličinu tog niza (n)
-        hitable_list(hitable **l, int n) { list = l; list_size = n; }
+        // Konstruktor koji prima niz pokazivača na hitable objekte i veličinu tog niza (n).
+        // Na GPU-u se ovi pokazivači obično inicijalizuju unutar setup kernela.
+        __device__ hitable_list(hitable **l, int n) {list = l; list_size = n; }
         
-        // Glavna metoda koja prolazi kroz sve objekte u listi i provjerava da li ih je zrak pogodio
-        virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const;
+        // Virtuelna funkcija za detekciju presjeka zraka sa bilo kojim objektom iz liste.
+        // Raspon [tmin, tmax] definiše interval parametra 't' unutar kojeg se presjek smatra validnim.
+        __device__ virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
         
-        hitable **list; // Dinamički niz pokazivača na objekte u sceni
-        int list_size;  // Broj objekata u našoj sceni
+        hitable **list; // Niz pokazivača na objekte koji implementiraju 'hitable' interfejs
+        int list_size;  // Broj objekata u listi
 };
 
-// Logika za prolazak kroz cijelu scenu.
-// Kada tražimo šta je zrak pogodio, 
-// moramo naći najbliži objekat ispred kamere.
-bool hitable_list::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
-    hit_record temp_rec;
-    bool hit_anything = false;
-    
-    // Na početku, naša najbliža tačka udara ('closest_so_far') je zapravo gornja granica (t_max).
-    // Kako budemo pronalazile pogotke koji su bliži, smanjivaćemo ovu granicu.
-    float closest_so_far = t_max;
-    
-    // Prolazimo petljom kroz apsolutno sve objekte koje smo ubacile u scenu
-    for (int i = 0; i < list_size; i++) {
-        // Provjeravamo sudar za trenutni objekat 'list[i]'.
-        // Ključni trik: umjesto fiksne granice, predajemo 'closest_so_far'.
-        // Na taj način, ako smo već pogodile nešto na udaljenosti npr. t=3, 
-        // potpuno ignorišemo sve druge objekte koji su dalje od toga (npr. t=5) jer su zaklonjeni.
-        if (list[i]->hit(r, t_min, closest_so_far, temp_rec)) {
-            hit_anything = true;
-            closest_so_far = temp_rec.t; // Ažuriramo granicu na novu, bližu tačku udara
-            rec = temp_rec;              // Kopiramo sve detalje o ovom (trenutno najbližem) pogotku
+// Implementacija funkcije za detekciju presjeka (hit) na GPU.
+// Kada zrak prolazi kroz scenu, moramo provjeriti presjek sa svim objektima,
+// ali prikazati samo onaj koji je najbliži početku zraka (closest intersection).
+__device__ bool hitable_list::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
+        hit_record temp_rec;
+        bool hit_anything = false;
+        
+        // 'closest_so_far' na početku postavljamo na gornju granicu intervala (t_max).
+        // Kako pronalazimo pogotke koji su bliži, ova granica se smanjuje. Na taj način osiguravamo
+        // da se provjeravaju samo oni presjeci koji su bliži od do sada najbiližeg pronađenog.
+        float closest_so_far = t_max;
+        
+        // Iteriramo kroz sve objekte u našoj sceni na GPU
+        for (int i = 0; i < list_size; i++) {
+            // Pozivamo 'hit' funkciju za pojedinačni objekat, prosljeđujući 'closest_so_far' kao novu gornju granicu.
+            if (list[i]->hit(r, t_min, closest_so_far, temp_rec)) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t; // Smanjujemo interval jer smo pronašli bliži presjek
+                rec = temp_rec;              // Ažuriramo hit_record sa detaljima o najbližem pogotku (tačka, normala, materijal)
+            }
         }
-    }
-    
-    // Vraćamo 'true' samo ako smo pogodile barem jedan objekat koji je vidljiv na ekranu
-    return hit_anything;
+        return hit_anything;
 }
 
 #endif
