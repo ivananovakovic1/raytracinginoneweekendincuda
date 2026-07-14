bc50ccf808f91de9a0cdf17c1b8d3331c14f96dc
ivananovakovic1 <ivananovakovic03@gmail.com>
2026-07-14 18:19:13 +0200

    Update sphere.h

--- sphere.h
+++ sphere.h
@@ -3,31 +3,54 @@
 
 #include "hitable.h"
 
-class sphere: public hitable  {
+// Ovdje nasljeđujemo našu baznu klasu 'hitable'. 
+// i tretiramo ih na isti način kad računamo pogotke zraka.
+class sphere: public hittable {
     public:
         sphere() {}
         sphere(vec3 cen, float r, material *m) : center(cen), radius(r), mat_ptr(m)  {};
-        virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
-        vec3 center;
-        float radius;
-        material *mat_ptr;
+        
+        // Ova metoda provjerava da li naš zrak siječe sferu.
+        virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const;
+        
+        vec3 center;       // Centar sfere u 3D prostoru
+        float radius;      // Poluprečnik sfere
+        material *mat_ptr; // Pokazivač na materijal od kog je sfera napravljena
 };
 
+// Ovdje pišemo samu logiku za računanje presjeka zraka i sfere.
 bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
-    vec3 oc = r.origin() - center;
+    vec3 oc = r.origin() - center; // Vektor od centra sfere do početka zraka
+    
+    // Računamo koeficijente za kvadratnu jednačinu.
+    // Primijetile smo da ovdje koristimo 'f' sufikse za brojeve (npr. 2.0f i slično u proračunima)
+    // kako ne bismo natjerale GPU da radi sa sporijim 'double' tipom podataka.
     float a = dot(r.direction(), r.direction());
     float b = dot(oc, r.direction());
     float c = dot(oc, oc) - radius*radius;
+    
+    // Računamo diskriminantu da vidimo da li zrak uopšte prolazi kroz sferu
     float discriminant = b*b - a*c;
+    
     if (discriminant > 0) {
-        float temp = (-b - sqrt(discriminant))/a;
+        // Tražimo prvo, bliže rješenje jednačine (tačku ulaska zraka u sferu)
+        float temp = (-b - sqrt(discriminant)) / a;
+        
+        // Provjeravamo da li je to rješenje unutar naših granica [t_min, t_max].
+        // Ovo nam je važno da izbjegnemo "shadow acne" i da ne renderujemo stvari iza kamere.
         if (temp < t_max && temp > t_min) {
             rec.t = temp;
-            rec.p = r.point_at_parameter(rec.t);
+            rec.p = r.point_at_parameter(rec.t); // Tačna 3D tačka udara
+            
+            // Računamo normalu u tački udara (vektor koji ide od centra sfere prema toj tački).
+            // Dijelimo sa radijusom da bismo dobili normalizovan vektor dužine 1.
             rec.normal = (rec.p - center) / radius;
-            rec.mat_ptr = mat_ptr;
+            
+            rec.mat_ptr = mat_ptr; // Pamti se materijal za sjenčenje
             return true;
         }
+        
+        // Ako je prvo rješenje ispalo van granica, provjeravamo ono drugo (dalje) rješenje
         temp = (-b + sqrt(discriminant)) / a;
         if (temp < t_max && temp > t_min) {
             rec.t = temp;
@@ -37,11 +60,8 @@ bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const 
             return true;
         }
     }
+    // Ako nema rješenja kvadratne jednačine, zrak je promašio sferu
     return false;
 }
 
-
 #endif
-
-
-
