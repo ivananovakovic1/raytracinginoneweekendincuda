6bda7c01bb66ae7813730254e7369e4c3671f403
ivananovakovic1 <ivananovakovic03@gmail.com>
2026-07-14 19:40:18 +0200

    Update vec3.h

--- vec3.h
+++ vec3.h
@@ -7,41 +7,44 @@
 
 // Klasa vec3 nam služi za sve u ovom projektu: 
 // predstavlja 3D tačke, 3D vektore pravca, ali i RGB boje (gdje su x, y, z zapravo r, g, b).
+// '__host__ __device__': ovo govori kompajleru da napravi dvije verzije koda.
+// Jedna se vrti na procesoru (host), a druga ide pravo na grafičku (device). 
+// Bez ovoga, grafička ne bi imala pojma šta je vec3.
 class vec3  {
 public:
-    vec3() {}
-    vec3(float e0, float e1, float e2) { e[0] = e0; e[1] = e1; e[2] = e2; }
+    __host__ __device__ vec3() {}
+    __host__ __device__ vec3(float e0, float e1, float e2) { e[0] = e0; e[1] = e1; e[2] = e2; }
     
     // Inline funkcije za lak pristup koordinatama ili komponentama boje
-    inline float x() const { return e[0]; }
-    inline float y() const { return e[1]; }
-    inline float z() const { return e[2]; }
-    inline float r() const { return e[0]; }
-    inline float g() const { return e[1]; }
-    inline float b() const { return e[2]; }
+    __host__ __device__ inline float x() const { return e[0]; }
+    __host__ __device__ inline float y() const { return e[1]; }
+    __host__ __device__ inline float z() const { return e[2]; }
+    __host__ __device__ inline float r() const { return e[0]; }
+    __host__ __device__ inline float g() const { return e[1]; }
+    __host__ __device__ inline float b() const { return e[2]; }
 
     // Osnovni unarni operatori
-    inline const vec3& operator+() const { return *this; }
-    inline vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
-    inline float operator[](int i) const { return e[i]; }
-    inline float& operator[](int i) { return e[i]; };
+    __host__ __device__ inline const vec3& operator+() const { return *this; }
+    __host__ __device__ inline vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+    __host__ __device__ inline float operator[](int i) const { return e[i]; }
+    __host__ __device__ inline float& operator[](int i) { return e[i]; };
 
     // Vektorski operatori sa dodjelom vrijednosti
-    inline vec3& operator+=(const vec3 &v2);
-    inline vec3& operator-=(const vec3 &v2);
-    inline vec3& operator*=(const vec3 &v2);
-    inline vec3& operator/=(const vec3 &v2);
-    inline vec3& operator*=(const float t);
-    inline vec3& operator/=(const float t);
+    __host__ __device__ inline vec3& operator+=(const vec3 &v2);
+    __host__ __device__ inline vec3& operator-=(const vec3 &v2);
+    __host__ __device__ inline vec3& operator*=(const vec3 &v2);
+    __host__ __device__ inline vec3& operator/=(const vec3 &v2);
+    __host__ __device__ inline vec3& operator*=(const float t);
+    __host__ __device__ inline vec3& operator/=(const float t);
 
     // Funkcije za računanje dužine vektora.
     // Primijetile smo da često koristimo 'squared_length' (kvadrat dužine) jer je 
     // računski mnogo jeftiniji od obične dužine (ne zahtijeva skupu operaciju korjenovanja 'sqrt').
-    inline float length() const { return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]); }
-    inline float squared_length() const { return e[0]*e[0] + e[1]*e[1] + e[2]*e[2]; }
+    __host__ __device__ inline float length() const { return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]); }
+    __host__ __device__ inline float squared_length() const { return e[0]*e[0] + e[1]*e[1] + e[2]*e[2]; }
     
     // Funkcija koja pretvara vektor u jedinični vektor (vektor dužine 1)
-    inline void make_unit_vector();
+    __host__ __device__ inline void make_unit_vector();
 
     float e[3]; // Tri komponente našeg vektora
 };
@@ -58,93 +61,93 @@ inline std::ostream& operator<<(std::ostream &os, const vec3 &t) {
     return os;
 }
 
-inline void vec3::make_unit_vector() {
+__host__ __device__ inline void vec3::make_unit_vector() {
     float k = 1.0 / sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
     e[0] *= k; e[1] *= k; e[2] *= k;
 }
 
 // Preklapanje operatora za sabiranje, oduzimanje, množenje i dijeljenje vektora.
 // Sve je napisano inline kako bi kompajler mogao maksimalno da optimizuje kod tokom izvršavanja.
-inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
+__host__ __device__ inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
     return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
 }
 
-inline vec3 operator-(const vec3 &v1, const vec3 &v2) {
+__host__ __device__ inline vec3 operator-(const vec3 &v1, const vec3 &v2) {
     return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);
 }
 
 // Množenje dva vektora po komponentama (koristi se npr. za miješanje boja materijala i svjetlosti)
-inline vec3 operator*(const vec3 &v1, const vec3 &v2) {
+__host__ __device__ inline vec3 operator*(const vec3 &v1, const vec3 &v2) {
     return vec3(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v2.e[2]);
 }
 
-inline vec3 operator/(const vec3 &v1, const vec3 &v2) {
+__host__ __device__ inline vec3 operator/(const vec3 &v1, const vec3 &v2) {
     return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);
 }
 
 // Množenje i dijeljenje vektora skalarom (brojem)
-inline vec3 operator*(float t, const vec3 &v) {
+__host__ __device__ inline vec3 operator*(float t, const vec3 &v) {
     return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
-inline vec3 operator/(vec3 v, float t) {
+__host__ __device__ inline vec3 operator/(vec3 v, float t) {
     return vec3(v.e[0]/t, v.e[1]/t, v.e[2]/t);
 }
 
-inline vec3 operator*(const vec3 &v, float t) {
+__host__ __device__ inline vec3 operator*(const vec3 &v, float t) {
     return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
 // Skalarni proizvod dva vektora (dot product) – daje nam skalar.
 // Izuzetno nam je važan za računanje uglova između zraka i normala površine.
-inline float dot(const vec3 &v1, const vec3 &v2) {
+__host__ __device__ inline float dot(const vec3 &v1, const vec3 &v2) {
     return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1]  + v1.e[2] * v2.e[2];
 }
 
 // Vektorski proizvod dva vektora (cross product) – daje nam novi vektor 
 // koji je pod 90 stepeni u odnosu na oba početna vektora. Koristimo ga npr. za orijentaciju kamere.
-inline vec3 cross(const vec3 &v1, const vec3 &v2) {
+__host__ __device__ inline vec3 cross(const vec3 &v1, const vec3 &v2) {
     return vec3( (v1.e[1]*v2.e[2] - v1.e[2]*v2.e[1]),
                 (-(v1.e[0]*v2.e[2] - v1.e[2]*v2.e[0])),
                 (v1.e[0]*v2.e[1] - v1.e[1]*v2.e[0]));
 }
 
-inline vec3& vec3::operator+=(const vec3 &v){
+__host__ __device__ inline vec3& vec3::operator+=(const vec3 &v){
     e[0]  += v.e[0];
     e[1]  += v.e[1];
     e[2]  += v.e[2];
     return *this;
 }
 
-inline vec3& vec3::operator*=(const vec3 &v){
+__host__ __device__ inline vec3& vec3::operator*=(const vec3 &v){
     e[0]  *= v.e[0];
     e[1]  *= v.e[1];
     e[2]  *= v.e[2];
     return *this;
 }
 
-inline vec3& vec3::operator/=(const vec3 &v){
+__host__ __device__ inline vec3& vec3::operator/=(const vec3 &v){
     e[0]  /= v.e[0];
     e[1]  /= v.e[1];
     e[2]  /= v.e[2];
     return *this;
 }
 
-inline vec3& vec3::operator-=(const vec3 &v){
+__host__ __device__ inline vec3& vec3::operator-=(const vec3 &v){
     e[0]  -= v.e[0];
     e[1]  -= v.e[1];
     e[2]  -= v.e[2];
     return *this;
 }
 
-inline vec3& vec3::operator*=(const float t){
+__host__ __device__ inline vec3& vec3::operator*=(const float t){
     e[0]  *= t;
     e[1]  *= t;
     e[2]  *= t;
     return *this;
 }
 
-inline vec3& vec3::operator/=(const float t){
+__host__ __device__ inline vec3& vec3::operator/=(const float t){
     float k = 1.0 / t;
     
     e[0]  *= k;
@@ -154,7 +157,7 @@ inline vec3& vec3::operator/=(const float t){
 }
 
 // Funkcija koja vraća jedinični vektor (normalizacija)
-inline vec3 unit_vector(vec3 v) {
+__host__ __device__ inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 }
 
